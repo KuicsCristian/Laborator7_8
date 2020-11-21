ca9bec19012521dcc07d59f85d721545678fe636
KuicsCristian <47479919+KuicsCristian@users.noreply.github.com>
2020-11-21 12:37:30 +0200

    latest

--- src/App.js
+++ src/App.js
@@ -1,30 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Switch, Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Program from "./program";
-
-const lista = [
-  {
-    id: 1,
-    ora: "10:00",
-    titlu: "Întâlnire cu presa",
-    loc: "Sala G114",
-    descriere: "Nu este cazul"
-  },
-  {
-    id: 2,
-    ora: "11:00",
-    titlu: "Bem cafea",
-    loc: "Birou",
-    descriere: "Bem cafea de calitate, cumparata de la Coffee Corner."
-  }
-];
+import Formular from "./formular";
+import NotFound from "./notfound";
 
 export default function App() {
-  const stergActivitate = () => 1;
+  const [lista, setLista] = useState([]);
+
+  const stergActiv = (ev) => {
+    const lista1 = lista.filter((item) => {
+      if (item.id === ev.target.id) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    setLista(lista1);
+    //console.log("Sterg: " + ev.target.id);
+  };
+
+  const adaugaActiv = (act) => {
+    act.id = lista.length + 1;
+    setLista([...lista, act]);
+  };
+
+  const stil = {
+    container: { maxWidth: "700px" }
+  };
 
   return (
-    <Container>
-      <Program activitati={lista} sterg={stergActivitate} />
+    <Container style={stil.container}>
+      <Navbar bg="primary" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/">
+              Program
+            </Nav.Link>
+            <Nav.Link as={Link} to="/formular">
+              Formular
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Switch>
+        <Route exact path="/">
+          <Program activitati={lista} sterge={stergActiv} />
+        </Route>
+        <Route path="/formular">
+          <Formular transmit={adaugaActiv} />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
     </Container>
   );
 }
---
+++ src/notfound.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+  return <h4>Not found! (404)</h4>;
+};
+
+export default NotFound;
